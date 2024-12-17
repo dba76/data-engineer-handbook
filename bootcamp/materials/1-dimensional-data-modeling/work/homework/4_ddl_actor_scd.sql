de4cd5decc2d1535dbcd647bf0486b813f19960f
Sameer Bhandari <sameer@mcssoftwaresolutions.com>
2024-12-17 17:15:15 +0000

    fixed homework

--- bootcamp/materials/1-dimensional-data-modeling/work/homework/4_ddl_actor_scd.sql
+++ bootcamp/materials/1-dimensional-data-modeling/work/homework/4_ddl_actor_scd.sql
@@ -1,11 +1,12 @@
 -- CREATE TYPE quality_class as ENUM('star', 'good', 'average', 'bad');
 
-CREATE TABLE if not EXISTS actors_history_scd (
+DROP TABLE actors_history_scd;
+
+CREATE TABLE actors_history_scd (
     actorid TEXT,
     actor TEXT,
     is_active BOOLEAN,
     quality_class quality_class,
     start_date INTEGER,
-    end_date INTEGER,
-    current_year INTEGER
+    end_date INTEGER
 )
--- bootcamp/materials/1-dimensional-data-modeling/work/homework/5_backfill_actors_history_scd.sql
+++ bootcamp/materials/1-dimensional-data-modeling/work/homework/5_backfill_actors_history_scd.sql
@@ -1,8 +1,42 @@
-select actorid, actor, quality_class, 
-    is_active, 
-    LAG(quality_class, 1) OVER (w) as previous_qc,
-    LAG(is_active, 1) OVER (w) as previous_is_active,
-    year as current_year 
-from actors
+-- INSERT INTO actors_history_scd 
+with history as (
+    select actorid,
+        actor,
+        quality_class,
+        is_active,
+        LAG(quality_class, 1) OVER (w) as previous_qc,
+        LAG(is_active, 1) OVER (w) as previous_is_active,
+        year as current_year
+    from actors WINDOW w as (
+            PARTITION BY actorid
+            ORDER BY year
+        )
+),
+indicator AS (
+    select *,
+        CASE
+            WHEN quality_class <> previous_qc
+            or is_active <> previous_is_active THEN 1
+            ELSE 0
+        END as change_indicator
+    from history
+),
+with_streaks as (
+    select *,
+        SUM(change_indicator) OVER(
+            PARTITION BY actorid
+            ORDER BY current_year
+        ) as streak_identifier
+    from indicator
+)
+select actorid,
+    MAX(actor) as actor,
+    is_active,
+    quality_class,
+    MIN(current_year) as start_date,
+    MAX(current_year) as end_date
+from with_streaks
 -- where actorid = 'nm0000366'
-WINDOW w as (PARTITION BY actorid ORDER BY year)
+GROUP by actorid,
+    is_active,
+    quality_class
